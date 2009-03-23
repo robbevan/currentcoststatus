ec20be14052768db1e693cc473721974c2ea05dd
Rob Bevan <robbevan@xpt.com>
2009-03-23 18:23:53 +0000

    initial RubyCocoa Xcode project and gitignore

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  CurrentCostStatus
+//
+//  Created by Rob Bevan on 23/03/2009.
+//  Copyright (c) 2009 robbevan.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
